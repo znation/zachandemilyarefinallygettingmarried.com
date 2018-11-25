a70c1bde8a2cf448e3e8344112389985586e64c7
Zach Nation <znation@gmail.com>
2018-11-25 15:33:20 -0800

    Add Google Maps embed

--- src/pages/the-party.js
+++ src/pages/the-party.js
@@ -5,9 +5,13 @@ import { PageLayout } from '../components/layout'
 
 const SecondPage = () => (
   <PageLayout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <iframe
+      width="600"
+      height="450"
+      frameBorder="0"
+      style={{border:0}}
+      src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc8v5UsYVkFQRmARnp0afFmI&key=AIzaSyC7yYPJolqdd0I01einRXIWw014fu_sevQ"
+      allowFullScreen />
   </PageLayout>
 )
 
