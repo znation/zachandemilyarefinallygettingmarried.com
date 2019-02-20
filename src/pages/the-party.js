dcdc9422113e4a0b9e17c57d568948d26e12e71c
Zach Nation <znation@apple.com>
2019-02-19 19:12:44 -0800

    Implement "The Party" Page

--- src/pages/the-party.js
+++ src/pages/the-party.js
@@ -1,17 +1,68 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import { PageLayout } from '../components/layout'
 
+const Image = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        fullImage: file(relativePath: { eq: "pavillionmosiac.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 960) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Img fluid={data.fullImage.childImageSharp.fluid} style={{
+        width: "100%",
+        marginBottom: 20
+      }}/>
+    )}
+  />
+)
+
 const SecondPage = () => (
   <PageLayout>
+    <p>
+      We will be getting married in a private ceremony at the courthouse with our parents and siblings
+      in attendance. Afterwards, we’d love for you to join us to celebrate at the&nbsp;
+      <a href="https://www.stoneburnerseattle.com/the-olympic-rooftop-pavilion">
+        Olympic Rooftop Pavilion
+      </a> in Seattle, Washington at 5pm on Sunday, August 25, 2019.
+    </p>
+    <p>
+      Please join us for cocktail hour on the roof, followed by a family style dinner prepared by local
+      restaurant Stoneburner, and then pie and cake. And games. Lots of games.
+    </p>
+    <Image />
     <iframe
-      width="600"
-      height="450"
+      title="Map to Venue"
+      width="100%"
+      height="400"
       frameBorder="0"
       style={{border:0}}
       src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc8v5UsYVkFQRmARnp0afFmI&key=AIzaSyC7yYPJolqdd0I01einRXIWw014fu_sevQ"
       allowFullScreen />
+    <p>
+      Street parking can be challenging. However, the hotel has underground parking, and parking is
+      also available at several nearby pay lots:
+    </p>
+    <ul>
+      <li>
+        Republic Parking Lot: 5433 Leary Ave NW, Seattle, WA 98107
+      </li>
+      <li>
+        Diamond Parking Lot: 5447 Leary Ave NW, Seattle, WA 98107
+      </li>
+      <li>
+        Unidentified Parking Lot: 5444 Ballard Ave NW, Seattle, WA 98107
+      </li>
+    </ul>
   </PageLayout>
 )
 
