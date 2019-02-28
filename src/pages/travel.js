54ac7930d17cf0e5658476b3b0837a2c036945f4
Zach Nation <znation@apple.com>
2019-02-27 20:19:27 -0800

    Implement "under construction" travel page

--- src/pages/travel.js
+++ src/pages/travel.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import { PageLayout } from '../components/layout'
 
 const SecondPage = () => (
   <PageLayout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>🚧 Under Construction 🚧</h1>
+    <p>Coming Soon</p>
   </PageLayout>
 )
 
