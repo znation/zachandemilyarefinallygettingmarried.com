e5de70d920c18bb2275c7dcf5b2f0008e94a6746
Zach Nation <znation@apple.com>
2019-02-27 20:41:49 -0800

    Add picture of Seven to the RSVP page

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
 import { PageLayout } from '../components/layout'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+const ImageRow = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        seven: file(relativePath: { eq: "Seven.jpg" }) {
+          childImageSharp {
+            fixed(width: 535, height: 423) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Img fixed={data.seven.childImageSharp.fixed} />
+    )}
+  />
+)
 
 const SecondPage = () => (
   <PageLayout>
     <h1>RSVP</h1>
     <p>We'll be sending out paper invitations in early June and will have a special link to RSVP online. Until then, here's a cute photo of our cat, Seven!</p>
     <p>
+      <ImageRow />
     </p>
   </PageLayout>
 )
