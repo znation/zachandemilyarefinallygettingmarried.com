eb9c2d953ceed028886c4c8fd418a83ff697eccb
Zach Nation <znation@apple.com>
2019-02-27 20:18:46 -0800

    Add registry page

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import { PageLayout } from '../components/layout'
 
 const SecondPage = () => (
   <PageLayout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>
+      Please know that celebrating with us is gift enough. However, for friends and family that have been asking for gift ideas, we'd love for you to consider a <a href="https://www.apopo.org/en/donate">donation to APOPO</a>. We've also created an <a href="https://www.amazon.com/wedding/emily-mcclory-zachary-nation-seattle-august-2019/registry/3PVPP149B1PTB">online registry at Amazon</a>.
+    </p>
   </PageLayout>
 )
 
