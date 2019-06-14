83009f2554f7f5af77bf0019f53e6918657a0b14
Zach Nation <znation@apple.com>
2019-06-13 22:22:37 -0700

    Update Travel page

--- src/pages/travel.js
+++ src/pages/travel.js
@@ -4,8 +4,31 @@ import { PageLayout } from '../components/layout'
 
 const SecondPage = () => (
   <PageLayout>
-    <h1>🚧 Under Construction 🚧</h1>
-    <p>Coming Soon</p>
+    <h3>Lodging</h3>
+    <p>
+      Seattle is divided into many different neighborhoods.  We'd suggest staying downtown if you want to walk to the main tourist attractions.  They're also walkable from Belltown, which is in between downtown and the space needle and a bunch of restaurants and bars.  If you're into nightlife, Capitol Hill is a good fit and has easy access to downtown via light rail.  Ballard (where we live) is Scandinavian and has a cute mini-downtown.  Fremont is quirky and quiet. The University District has a bunch of shops and restaurants and also has light rail to downtown from the University. West Seattle is a little further but is on the water.  Most or all of these neighborhoods have hotels.  Seattle also has a large number of rooms or homes for rent on airbnb.com. 
+    </p>
+    <h3>
+      Transportation
+    </h3>
+    <p>
+      Uber and Lyft are very popular here.  Public transit is pretty good--buses can get you to most parts of the city (perhaps with a little walking) and we also have light rail from the airport, through downtown, to the University District.  Or you can bike! (There are bikes scattered all around the city with instructions on how to rent them.)  Driving is also an option but traffic can get bad depending upon the time of day.
+    </p>
+    <h3>
+      What to do
+    </h3>
+    <p>
+      Seattle has a lot to offer, especially in the summer.  Here are some of our favorite things:
+    </p>
+    <ul>
+      <li>Kayaking on Lake Washington (you can rent kayaks by the University)</li>
+      <li>Visiting Pike Place Market (free samples at Beecher's Cheese!)</li>
+      <li>Visiting the library (it's has a really interesting design--check out the red floor)</li>
+      <li>Going to the observation deck of the Columbia Tower (this way, you get a high of view of Seattle that includes the Space Needle)</li>
+      <li>Taking a ferry to Bainbridge Island</li>
+      <li>Hanging out at the beach (Golden Gardens or West Seattle)</li>
+      <li>Visiting the Fremont Troll (it's under a bridge) - while you're there, check out the Lenin statue!</li>
+    </ul>
   </PageLayout>
 )
 
