6cd56005ec076ed1e7fda25c7aa4289493b825cc
Zach Nation <znation@apple.com>
2019-07-23 22:37:38 -0700

    Add West Elm link

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -5,7 +5,15 @@ import { PageLayout } from '../components/layout'
 const SecondPage = () => (
   <PageLayout>
     <p>
-      Please know that celebrating with us is gift enough. However, for friends and family that have been asking for gift ideas, we'd love for you to consider a <a href="https://www.apopo.org/en/donate">donation to APOPO</a>. We've also created an <a href="https://www.amazon.com/wedding/emily-mcclory-zachary-nation-seattle-august-2019/registry/3PVPP149B1PTB">online registry at Amazon</a>.
+      Please know that celebrating with us is gift enough. However, for friends
+      and family that have been asking for gift ideas, we'd love for you to
+      consider a <a href="https://www.apopo.org/en/donate">donation to APOPO</a>.
+      We've also created an online registry at&nbsp;
+      <a href="https://www.amazon.com/wedding/emily-mcclory-zachary-nation-seattle-august-2019/registry/3PVPP149B1PTB">
+        Amazon
+      </a> and <a href="https://www.westelm.com/registry/txqfj922r5/registry-list.html">
+        West Elm
+      </a>.
     </p>
   </PageLayout>
 )
